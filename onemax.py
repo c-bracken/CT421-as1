22d57482c109a3841ca2232ae03548fb1755e5e2
Dion Collins <d.collins29@universityofgalway.ie>
2024-01-30 13:14:00 +0000

    Added Comments

--- onemax.py
+++ onemax.py
@@ -1,19 +1,25 @@
 import random
 
+# Number of individuals in the population
 population = 20
+# Length of each string
 length = 30
-# should be population / 4
+# Number of elite individuals to be in each generation
 elites = 5
-strlist = []
-fitnesses = []
+# List to store population strings
+popList = []
+# List to store fitnesses with corresponding strong
+fitnessList = []
 
 crossover_rate = 0.6
 mutation_rate = 0.1
 
+# Calculates fitness for every string in population
 def calculate_fitnesses(strings):
     for i in range(population):
-        fitnesses[i] = calc_fitness(strings[i])
+        fitnessList[i] = calc_fitness(strings[i])
 
+# Calculates fitness of individual strings
 def calc_fitness(string):
     fit = 0
     for i in range(length):
@@ -21,12 +27,14 @@ def calc_fitness(string):
             fit += 1
     return fit
 
+# Average Fitness
 def fitness_avg():
     fit_total = 0
-    for i in fitnesses:
+    for i in fitnessList:
         fit_total += i
     return fit_total / population
 
+# Performs mutations in a string
 def mutate(string):
     for i in range(length):
         if random.random() < mutation_rate:
@@ -36,29 +44,36 @@ def mutate(string):
                 string = string[:i] + "1" + string[(i + 1):]
     return string
 
+# Performs crossover between two parent strings
+# Returns two child strings
 def crossover(parent1, parent2):
     child1 = parent1
     child2 = parent2
     pivot = 0
     if random.random() < crossover_rate:
         pivot = random.randrange(1, length - 1)
+        # Head of parent1, tail of parent 2
         child1 = parent1[:pivot] + parent2[pivot:]
+        # Head of parent2, tail of parent 1
         child2 = parent2[:pivot] + parent1[pivot:]
     return [child1, child2]
 
+# Sort population by fitness
 def fitness_sort(strings):
     mockpop = list(range(population))
     for i in range(population):
-        mockpop[i] = [mockpop[i], fitnesses[i]]
+        mockpop[i] = [mockpop[i], fitnessList[i]]
     mockpop.sort(key=fitsort)
     return mockpop
 
 def fitsort(e):
     return e[1]
 
+# New population with elite individuals
+# Performs crossover, mutation
 def generate_new_pop(strings):
     newpop = []
-    elite_strs = fitness_sort(strings)[-elites:]
+    elite_strs = fitness_sort(strings)[int(-elites):]
     for i in range(elites):
         elite_strs[i] = strings[elite_strs[i][0]]
     for i in range(elites):
@@ -70,31 +85,39 @@ def generate_new_pop(strings):
     calculate_fitnesses(newpop)
     return newpop
 
+# Generates new populations until a solution is found
 for i in range(population):
     bitstr = ""
     for j in range(length):
         char = random.randint(0, 1)
         bitstr += "{}".format(char)
-    strlist.append(bitstr)
+    popList.append(bitstr)
     
-    fitnesses.append(0)
-calculate_fitnesses(strlist)
+    fitnessList.append(0)
+calculate_fitnesses(popList)
 
 averages = open("onemax_averages.csv", "w")
 averages.write("generation,average\n")
 
+# Counts number of generations
 generations = 0
+metrics = [generations, fitness_avg()]
+averages.write("{},{}\n".format(metrics[0], metrics[1]))
+print("generation:\t{}".format(metrics[0]))
+print("fitness average:\t{}".format(metrics[1]))
+# Prints information about each generation
 while True:
-    strlist = generate_new_pop(strlist)
+    popList = generate_new_pop(popList)
     generations += 1
     metrics = [generations, fitness_avg()]
     averages.write("{},{}\n".format(metrics[0], metrics[1]))
     print("generation:\t{}".format(metrics[0]))
     print("fitness average:\t{}".format(metrics[1]))
-    champ = fitness_sort(strlist)[-1]
+    champ = fitness_sort(popList)[-1]
     if champ[1] == length:
-        print("FREAK FOUND: INDEX {}, FREAK CODE {}".format(champ[0], strlist[champ[0]]))
+        print("FREAK FOUND: INDEX {}, FREAK CODE {}".format(champ[0], popList[champ[0]]))
         break
     else:
         print("NOT DONE")
+
 averages.close()
